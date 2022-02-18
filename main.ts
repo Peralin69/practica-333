4d2f44b9986099d34ef306dcd1a9b4b47571290f
Peralin69 <99762461+Peralin69@users.noreply.github.com>
2022-02-18 07:35:32 +0100

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,25 @@
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    music.playMelody("F C5 F E F C5 F E ", 1503)
+})
+input.onButtonPressed(Button.A, function () {
+    for (let index = 0; index < 10; index++) {
+        maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
+        basic.pause(500)
+        maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
+        maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOn)
+        basic.pause(500)
+        maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOff)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    for (let index = 0; index < 5; index++) {
+        music.playTone(370, music.beat(BeatFraction.Whole))
+        basic.pause(200)
+    }
+})
+basic.forever(function () {
+    basic.showIcon(IconNames.Heart)
+    basic.pause(200)
+    basic.showIcon(IconNames.SmallHeart)
+    basic.pause(200)
+})
